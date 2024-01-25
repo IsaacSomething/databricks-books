06de7b6bc4c4a377ac25c345971c2ebdaf6eb965
IsaacSomething <IsaacSomething>
2024-01-25 08:52:16 +0000

    more init

---
+++ 01 - Modeling Data Management Solutions/01 - Bronze Ingestion Patterns.sql
@@ -0,0 +1,9 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC # Bronze Ingestion Patterns
+-- MAGIC <br />
+-- MAGIC <img src="https://raw.githubusercontent.com/IsaacSomething/databricks-books/8cd88346aa040d5868b3138c76f116dca632fd1a/resources/01-model.png" alt="Data model" width="800" height="auto">
+
+-- COMMAND ----------
+
+
